2e3d6fa6316226ee0303f5e217a614b0d4ebfba7
Dominic Meddick <dominicmeddick@gmail.com>
2022-01-27 09:42:26 +0000

    ProductsController now returns an array of all the Products in the ModelBuilderExtensions Class

--- HPlusSport.API/Controllers/ProductsController.cs
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HPlusSport.API.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,9 +12,17 @@ namespace HPlusSport.API.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        private readonly ShopContext _context;
+
+        public ProductsController(ShopContext context) {
+
+            _context = context;
+
+            _context.Database.EnsureCreated();
+        }
         [HttpGet]
-        public string GetProducts() {
-            return "OK.";
+        public IEnumerable<Product> GetAllProducts() {
+            return _context.Products.ToArray();
         }
     }
 }
